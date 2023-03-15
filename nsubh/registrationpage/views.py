260ff6acae331d0a9dac3144ce4d5689fa1b0219
LaVaughnGit <113532445+LaVaughnGit@users.noreply.github.com>
2023-03-15 15:13:19 -0400

    Added basic Mongo changes

--- nsubh/registrationpage/views.py
+++ nsubh/registrationpage/views.py
@@ -1,10 +1,43 @@
-from django.shortcuts import render
+import datetime
+import dbm
+from django.shortcuts import redirect
 from django.template import loader
 from django.http import HttpResponse
+from pymongo import MongoClient
 
-# Create your views here.
+# Initializing Database
+USERNAME = ''
+client = MongoClient('mongodb://localhost:27017')
+NSUBH = client['NSUBH']
 
 
 def register(request):
-    template = loader.get_template('registrationpage.html')
-    return HttpResponse(template.render())
+    registerPage = loader.get_template('registrationpage.html')
+    if request.method == 'POST':
+        username = request.form['usernmae']
+        password = request.form['password']
+        year = request.form['year']
+        error = None
+
+        if not username:
+            error = "Username is required."
+        elif not password:
+            error = "Password is required."
+
+        if error is None:
+            try:
+                logincollection = NSUBH['Login']
+                USERNAME = username
+                information = {
+                    "Username:": username,
+                    "Password": password,
+                    "Year": year,
+                    "Date Created": datetime.datetime.utcnow
+                }
+                document = logincollection.insert_one(information).inserted_id
+            except dbm.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect('/loginpage/')
+
+    return HttpResponse(registerPage.render())
