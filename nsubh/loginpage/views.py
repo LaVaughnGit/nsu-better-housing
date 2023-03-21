398f24260a3a0273e048ae5e9788bfbbde6ca0fa
LaVaughnGit <113532445+LaVaughnGit@users.noreply.github.com>
2023-03-20 20:11:51 -0400

    Register to login page routing fixed

--- nsubh/loginpage/views.py
+++ nsubh/loginpage/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
+from pymongo import MongoClient
 
 
 @csrf_exempt
 def loginpage(request):
-    template = loader.get_template('loginpage.html')
-    return HttpResponse(template.render())
+    # Check the request method
+        
+    return render(request, 'loginpage.html')
