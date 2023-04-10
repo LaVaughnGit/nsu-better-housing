58865096e3b3ce68dabda50d896478d0b2f3dbdb
LaVaughnGit <113532445+LaVaughnGit@users.noreply.github.com>
2023-04-10 15:55:28 -0400

    Small changes MakoRS page

--- nsubh/reviewpage/views.py
+++ nsubh/reviewpage/views.py
@@ -6,9 +6,12 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def reviewpage(request):
-    if request.method == 'POST':
+    context = {}
+    if request.method == 'GET':
+
         context = {
             'username': 'testing',
             'nnumber': '12345678',
+            'email': '123@nova.edu',
         }
     return render(request, 'reviewpage.html', context)
